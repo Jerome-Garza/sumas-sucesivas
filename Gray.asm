b22cd27f070525fb99fa911d7d9263010572226a
Jerome-Garza <64096679+Jerome-Garza@users.noreply.github.com>
2020-05-08 17:09:14 -0500

    Update Gray.asm

--- Gray.asm
+++ Gray.asm
@@ -1,13 +1,13 @@
 #include "p16F628a.inc"    ;incluir librerias relacionadas con el dispositivo
  __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _BOREN_OFF & _LVP_OFF & _CPD_OFF & _CP_OFF    
-;configuración del dispositivotodo en OFF y la frecuencia de oscilador
+;configuraciÃ³n del dispositivotodo en OFF y la frecuencia de oscilador
 ;es la del "reloj del oscilador interno" (INTOSCCLK)     
 RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
 ; TODO ADD INTERRUPTS HERE IF USED
 MAIN_PROG CODE                      ; let linker place main program
 ;variables para el contador:
- i equ 0x33
+ i equ 0x30
 ;inicio del programa: 
 START
  
